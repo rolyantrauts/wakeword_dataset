9a63a20fe1188474bde066a5cb1282139b8e4c58
rolyantrauts <rolyantrauts@gmail.com>
2026-03-30 13:18:32 +0000

    Add files via upload

---
+++ install_emotivoice.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/netease-youdao/EmotiVoice.git
+cd EmotiVoice
+conda create -n EmotiVoice python=3.8 -y
+conda activate EmotiVoice
+pip install torch torchaudio
+pip install numpy numba scipy transformers soundfile yacs g2p_en jieba pypinyin pypinyin_dict
+python -m nltk.downloader "averaged_perceptron_tagger_eng"
+
+git lfs install
+git clone https://www.modelscope.cn/syq163/outputs.git
